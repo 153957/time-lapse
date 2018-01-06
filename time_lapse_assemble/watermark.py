c86786a0a8340c04da219eb1f0f9635af58bdf55
Arne de Laat <arne@delaat.net>
2018-01-06 15:28:55 +0100

    Add watermark font.

--- time_lapse_assemble/watermark.py
+++ time_lapse_assemble/watermark.py
@@ -2,10 +2,21 @@ import os
 
 import ffmpeg
 
+RENNER_FONT = os.path.join(os.path.dirname(__file__), 'fonts/Renner-Book.ttf')
+FONT_OPTIONS = {
+    'fontfile': RENNER_FONT,
+    'fontcolor': 'white',
+    'shadowcolor': 'black'
+}
 
-def watermark():
-    watermark = (ffmpeg
-        .input(os.join(__file__, ...))
+
+def add_watermark(input):
+    watermarked_input = (input
+        .filter_(
+            'drawtext', text='Arne de Laat', fontsize=20, **FONT_OPTIONS
+        )
+        .filter_(
+            'drawtext', text='153957 Photography', fontsize=16, **FONT_OPTIONS
+        )
     )
-
-    return watermark
+    return watermarked_input
