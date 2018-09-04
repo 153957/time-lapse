cab866c0a4eb9fa7c27c184d60fd2b8d5bd8ef5c
Arne de Laat <arne@delaat.net>
2018-09-04 08:25:54 +0200

    Add new movie of wedding ceremony and reception.

---
+++ scripts/D700_180901_1_2.py
@@ -0,0 +1,24 @@
+import ffmpeg
+
+from time_lapse import output
+
+NAME = 'D700_180901_1_2'
+PATTERNS = [
+    '/Volumes/Falcon/tl_temp/180901/1/*.tiff',  # ADL_242138 - ADL_242499
+    '/Volumes/Falcon/tl_temp/180901/2/*.tiff',  # ADL_242506 - ADL_243212
+]
+
+def make_movie():
+    inputs = [
+        ffmpeg
+        .input(pattern, pattern_type='glob', framerate=10)
+        .filter_('deflicker', mode='pm', size=2)
+        for pattern in PATTERNS
+    ]
+    input = ffmpeg.concat(*inputs)
+
+    output.create_outputs(input, NAME, verbose=True)
+
+
+if __name__ == '__main__':
+    make_movie()
