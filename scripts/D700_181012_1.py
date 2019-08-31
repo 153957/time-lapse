448a29edf5eb4d23b42d92510839972585c524ce
Arne de Laat <arne@delaat.net>
2019-08-31 21:24:17 +0200

    Add new time-lapse movie of morning trains

---
+++ scripts/D700_181012_1.py
@@ -0,0 +1,25 @@
+import os
+
+import ffmpeg
+
+from time_lapse import output
+
+NAME = os.path.basename(__file__).replace('.py', '')
+PATTERNS = [
+    '/Volumes/Falcon/tl_temp/181012_1/*.tiff',  # ADL_243737 - ADL_244687
+]
+
+
+def make_movie():
+    inputs = [
+        ffmpeg
+        .input(pattern, pattern_type='glob', framerate=30)
+        for pattern in PATTERNS
+    ]
+    input = ffmpeg.concat(*inputs)
+
+    output.create_outputs(input, NAME, verbose=True)
+
+
+if __name__ == '__main__':
+    make_movie()
