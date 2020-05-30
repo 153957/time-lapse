dceb994ce45fe35f7618bfd4816dcc8d631c0c6c
Arne de Laat <arne@delaat.net>
2020-05-30 12:24:19 +0200

    Add script to detect audio streams
    Inspect files matching a pattern for audio streams,
    name of those with an audio stream are returned.

---
+++ time_lapse/detect_audio.py
@@ -0,0 +1,9 @@
+import ffmpeg
+import glob
+
+
+def files_with_audio(pattern='*.mp4'):
+    for filename in glob.glob(pattern):
+         for stream in ffmpeg.probe(filename)['streams']:
+            if stream['codec_type'] == 'audio':
+                yield filename
