a232c535bde5367315bc24f4e3f3bf19e96485a9
Arne de Laat <arne@delaat.net>
2020-06-06 16:35:08 +0200

    Disable watermark when using the CLI

--- time_lapse/cli.py
+++ time_lapse/cli.py
@@ -13,7 +13,7 @@ def make_movie(name, pattern, fps, deflicker):
     if deflicker:
         input = input.filter_('deflicker', mode='pm', size=deflicker)
 
-    output.create_outputs(input, name, verbose=True)
+    output.create_outputs(input, name, verbose=True, watermark=False)
 
 
 def main():
@@ -21,22 +21,26 @@ def main():
     parser.add_argument(
         '--name',
         default='output',
-        help='Output name of the movie, without file extension.')
+        help='Output name of the movie, without file extension.'
+    )
     parser.add_argument(
         '--pattern',
         default='*.tiff',
-        help='Regex pattern with which to find the input frames.')
+        help='Regex pattern with which to find the input frames.'
+    )
     parser.add_argument(
         '--fps',
         help='Frame rate of the created movie.',
         type=int,
         default=30,
-        choices=[1, 2, 6, 12, 15, 24, 25, 30, 48, 50, 60, 120, 240, 300])
+        choices=[1, 2, 6, 12, 15, 24, 25, 30, 48, 50, 60, 120, 240, 300]
+    )
     parser.add_argument(
         '--deflicker',
         help='Frame window to use for deflicker, off when set to 0.',
         type=int,
-        default=0)
+        default=0
+    )
     args = parser.parse_args()
 
     make_movie(args.name, args.pattern, args.fps, args.deflicker)
