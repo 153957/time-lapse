07a9fa37d588908e0ac77b0a8fae19d0fea0f5d4
Arne de Laat <arne@delaat.net>
2021-09-18 21:37:35 +0200

    Allow patterns to be a single string

--- time_lapse/source.py
+++ time_lapse/source.py
@@ -4,11 +4,14 @@ import ffmpeg
 def get_input(patterns, fps, deflicker):
     """Find input files and set framerate and deflickering
 
-    :param patterns: glob patterns to find input frames.
+    :param patterns: glob pattern(s) to find input frames.
     :param fps: framerate of the output video.
     :param deflicker: frame window to use for deflickering,
 
     """
+    if isinstance(patterns, str):
+        patterns = [patterns]
+
     return ffmpeg.concat(*[
         ffmpeg
         .input(pattern, pattern_type='glob', framerate=fps)
