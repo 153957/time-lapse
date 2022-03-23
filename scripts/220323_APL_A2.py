3c69b065877d0d22ac8fcfa530d7732c38edbed1
Arne de Laat <arne@delaat.net>
2022-03-23 20:57:55 +0100

    Add new time-lapse movie using the tblend filter

---
+++ scripts/220323_APL_A2.py
@@ -0,0 +1,20 @@
+import os
+
+from glob import glob
+
+from time_lapse import make_movie, output, source
+
+NAME = os.path.basename(__file__).replace('.py', '')
+PATTERN = '/Volumes/Jedi/Cache/220323_1_APL/*.tif',  # APL_103712 - APL_103931
+# poster: APL_103782
+
+
+if __name__ == '__main__':
+    source_input = (
+        source
+        .get_input(PATTERN, 24, 0)
+        .filter_('tblend', all_mode='lighten', all_opacity=0.33)
+        .filter_('tblend', all_mode='lighten', all_opacity=0.33)
+    )
+
+    output.create_outputs(source_input, NAME, watermark=True, verbose=False, dryrun=False)
