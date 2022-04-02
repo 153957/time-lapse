85906361d480bb8b5fb0c8ff2a256acb2bb330a0
Arne de Laat <arne@delaat.net>
2022-04-02 15:43:15 +0200

    Change output of check_interval to list sequences
    Output now shows the number of images in a sequence, the interval
    in that sequence and the first and last image paths.
    A sequence requires at least 5 images with a consistent interval.
    Use single quotes.

--- time_lapse/check_interval.py
+++ time_lapse/check_interval.py
@@ -7,6 +7,7 @@ from glob import glob
 import exifreader
 
 MARGIN = datetime.timedelta(seconds=0.2)
+MIN_IMAGES_SEQUENCE = 5
 
 
 def get_image_date(image_path):
@@ -18,6 +19,50 @@ def get_image_date(image_path):
     full_date_time = f'{date_time}.{subsec}'
     image_date = datetime.datetime.strptime(full_date_time, '%Y:%m:%d %H:%M:%S.%f')
     return image_date
+
+
+def find_sequences(pattern, shots_per_interval):
+    skip = shots_per_interval
+    files = sorted(glob(pattern))
+    image_dates = [
+        {'path': pathlib.Path(path).name, 'date': get_image_date(path)}
+        for path in files[::skip]
+    ]
+
+    n_same_interval = 0
+    start_of_sequence = image_dates[0]['path']
+
+    print(
+        '   n',
+        'interval',
+        'sequence',
+        sep='\t'
+    )
+
+    for i, (previous, current, following) in enumerate(zip(image_dates[:-2], image_dates[1:-1], image_dates[2:]), 1):
+        n_same_interval += 1
+
+        interval = current['date'] - previous['date']
+        new_interval = following['date'] - current['date']
+
+        if abs(interval - new_interval) > MARGIN:
+            if n_same_interval > MIN_IMAGES_SEQUENCE:
+                print(
+                    f'{n_same_interval:4}',
+                    f'{interval.total_seconds():7}s',
+                    f'{start_of_sequence} → {current["path"]}',
+                    sep='\t'
+                )
+            start_of_sequence = current['path']
+            n_same_interval = 0
+
+    if n_same_interval > MIN_IMAGES_SEQUENCE:
+        print(
+            f'{n_same_interval:4}',
+            f'{new_interval.total_seconds():7}s',
+            f'{start_of_sequence} → {following["path"]}',
+            sep='\t'
+        )
 
 
 def find_outliers(pattern, shots_per_interval):
@@ -38,10 +83,10 @@ def find_outliers(pattern, shots_per_interval):
         if previous_interval and abs(interval - previous_interval) > MARGIN:
             outliers.append((interval, previous['path'], current['path']))
             print(
-                f"{i:5} – "
-                f"{n_since_last_change:4} – "
-                f"{previous_interval.total_seconds():6}s → {interval.total_seconds():6}s – "
-                f"{previous['path']} → {current['path']}"
+                f'{i:5} – '
+                f'{n_since_last_change:4} – '
+                f'{previous_interval.total_seconds():6}s → {interval.total_seconds():6}s – '
+                f'{previous["path"]} → {current["path"]}'
             )
             n_since_last_change = 0
         previous_interval = interval
@@ -64,7 +109,7 @@ def main():
     )
     args = parser.parse_args()
 
-    find_outliers(
+    find_sequences(
         args.pattern,
         args.shots_per_interval,
     )
